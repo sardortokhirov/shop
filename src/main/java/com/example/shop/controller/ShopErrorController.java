11cbb39376b90974ee30e7749978f528e1a04396
= <=>
2023-05-13 08:03:24 -0700

    error page

---
+++ src/main/java/com/example/shop/controller/ShopErrorController.java
@@ -0,0 +1,17 @@
+package com.example.shop.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Date-5/13/2023
+ * Time-7:48 AM
+ */
+@Controller
+public class ShopErrorController implements ErrorController {
+    @GetMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+}
